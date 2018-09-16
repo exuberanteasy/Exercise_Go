50c769a1f3fc0d55e93baa6d08e0a070bc5cf1e7
exuberanteasy <exuberanteasy@gmail.com>
2018-09-17 06:52:05 +0800

    Update writeLine.cs

--- writeLine.cs
+++ writeLine.cs
@@ -8,3 +8,26 @@ static void Main(string[] args)
     Console.WriteLine("打八折後 總金額:{0}", price * qty * 0.8);
     Console.Read();
 }
+
+static void Main(string[] args)
+{
+    string str1;
+    int price, qty;
+    Console.WriteLine();
+    Console.WriteLine("  電腦圖書廣場");
+    Console.WriteLine("===========================");
+    Console.Write("  1.書名:");
+    str1 = Console.ReadLine();    //輸入書名並指定給str1變數。
+    Console.Write("  2.售價:");
+
+    //輸入售價並使用 int.Parse() 將輸入的資料轉成整數，在指定給price
+    price = int.Parse(Console.ReadLine());
+    Console.Write("  3.數量:");
+
+    //輸入數量並使用 Convert.ToInt32 將輸入的資料轉成整數，在指定給qty
+    qty = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("====================");
+    Console.WriteLine("  4.金額:{0}", price * qty);
+    Console.Read();
+}
