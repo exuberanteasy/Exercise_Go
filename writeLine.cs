2ed62c39ce61a7caf010fd82dcb4a98b6fbb72c2
exuberanteasy <exuberanteasy@gmail.com>
2018-09-17 06:41:22 +0800

    Update writeLine.cs

--- writeLine.cs
+++ writeLine.cs
@@ -1,1 +1,10 @@
 博碩 Chapter2_2-18
+
+static void Main(string[] args)
+{
+    int price = 120, qty = 50;
+    Console.WriteLine("售價:{0} 數量{1}", price, 50);
+    Console.WriteLine();
+    Console.WriteLine("打八折後 總金額:{0}", price * qty * 0.8);
+    Console.Read();
+}
